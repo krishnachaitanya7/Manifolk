c4d50c4c730e3b52df60b9f38039f7737149b970
Krishna <chaitanyaradon89@gmail.com>
2020-11-13 02:41:31 -0600

    remove waste comments

--- tsne_plots/tsne_plot.py
+++ tsne_plots/tsne_plot.py
@@ -2,7 +2,7 @@ import dash
 import plotly.express as px
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import dash_html_components as html
 from sqlite_db import SQLDb
 import math
